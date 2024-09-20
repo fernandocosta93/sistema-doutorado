eaa30af58601c6312d3857f3c3bb4421e8682d93
Fernando Costa <fhscosta0993@gmail.com>
2024-09-20 08:18:03 -0300

    Criando arquivos para TJSP, ainda vazios

--- functions/funcs_navegar.py
+++ functions/funcs_navegar.py
@@ -3,7 +3,7 @@ def variaveis_necessarias(nome_processo):
     img_inicio_processo = 'imgs/inicio_processo.png'
     img_fim_processo = 'imgs/fim_processo.png'
     img_seta_transicao = 'imgs/seta_transicao.png'
-    texto_seta_transicao = 'template_matching/anvisa/navegar_seta_transicao.txt'
+    texto_seta_transicao = 'template_matching/navegar_seta_transicao.txt'
 
     if nome_processo == "ANVISA":
 
@@ -21,18 +21,19 @@ def variaveis_necessarias(nome_processo):
         texto_transicao_loop = 'template_matching/anvisa/navegar_transicao_loop.txt'
 
     elif nome_processo == 'TJ-SP':
-        img_atividade = ''
-        texto_atividade = ''
-        img_atividade_inicial = ''
-        texto_atividade_inicial = ''
-        img_atividade_final = ''
-        texto_atividade_final = ''
-        img_transicao_atividades = ''
-        texto_transicao_atividade = ''
-        img_autotransicao = ''
-        texto_autotransicao = ''
-        img_transicao_loop = ''
-        texto_transicao_loop = ''
+
+        img_atividade = 'imgs/tjsp/atividade.png'
+        texto_atividade = 'template_matching/tjsp/texto_atividades.txt'
+        img_atividade_inicial = 'imgs/tjsp/atividade_inicial.png'
+        texto_atividade_inicial = 'template_matching/tjsp/texto_atividades_iniciais.txt'
+        img_atividade_final = 'imgs/tjsp/atividade_final.png'
+        texto_atividade_final = 'template_matching/tjsp/texto_atividades_finais.txt'
+        img_transicao_atividades = 'imgs/tjsp/transicao_atividades.png'
+        texto_transicao_atividade = 'template_matching/tjsp/navegar_transicao_atividades.txt'
+        img_autotransicao = 'imgs/tjsp/auto_transicao.png'
+        texto_autotransicao = 'template_matching/tjsp/navegar_auto_transicao.txt'
+        img_transicao_loop = 'imgs/tjsp/transicao_loop.png'
+        texto_transicao_loop = 'template_matching/tjsp/navegar_transicao_loop.txt'
 
     return img_inicio_processo, img_fim_processo, img_seta_transicao, texto_seta_transicao, img_atividade, texto_atividade, img_atividade_inicial, \
           texto_atividade_inicial, img_atividade_final, texto_atividade_final, img_transicao_atividades, texto_transicao_atividade, \
